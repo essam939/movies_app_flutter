c950471bbb1233b397a9f2c689e130c314a9ed0d
essam939 <mohamed.essam9393@gmail.com>
2024-09-03 15:28:32 +0300

    add responsive package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movieapp/core/services/injection.dart';
 import 'package:movieapp/splash_screen.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
- configureDependencies();
+  configureDependencies();
   runApp(const MyApp());
 }
 
@@ -16,10 +17,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Movies App',
       theme: ThemeData.light(),
-      darkTheme:  ThemeData.dark(),
+      darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+        ],
+      ),
     );
   }
 }
